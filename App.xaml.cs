de7c1b018e95819842822185b408b36b7a19146e
bettertoppi <betterechap@gmail.com>
2026-08-16 12:59:52 +0500

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,18 +1,8 @@
-using System;
 using System.Windows;
 
 namespace TopuLauncher
 {
     public partial class App : Application
     {
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            var app = new App();
-            app.InitializeComponent();
-            
-            var mainWindow = new MainWindow();
-            app.Run(mainWindow);
-        }
     }
 }
